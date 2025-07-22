71f2454dfc1cba500aae07b93ec59aa2ce216c3b
Arjunyadavsoma <115440056+Arjunyadavsoma@users.noreply.github.com>
2025-07-22 19:47:46 +0530

    lasss

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,45 +1,35 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("com.google.gms.google-services") 
-     id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.my_webapp"
-    compileSdk = 34
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_webapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.1")
+        classpath("com.google.gms:google-services:4.4.0")
     }
 }
 
-flutter {
-    source = "../.."
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
